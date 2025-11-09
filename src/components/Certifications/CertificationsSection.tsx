d73b491d64e9ab60a586c25c85115fbd0cb4e32e
Vansh <vanshsoniofficial@gmail.com>
2025-11-10 01:05:02 +0530

    link issue

--- src/components/Certifications/CertificationsSection.tsx
+++ src/components/Certifications/CertificationsSection.tsx
@@ -28,13 +28,13 @@ const featured: Cert[] = [
     title: 'Azure AI Fundamentals', 
     issuer: 'neo4j', 
     year: 'November 2025',
-    proofUrl: 'https://graphacademy.neo4j.com/c/757eeadf-22c7-473d-ac26-f21797a40206/'
+    proofUrl: 'https://www.udemy.com/certificate/UC-c1f0eb7a-25f4-49db-812e-75293f2e939d/'
   },
   { 
     title: 'Neo4j Certified Professional', 
     issuer: 'Udemy', 
     year: 'November 2025',
-    proofUrl: 'https://www.udemy.com/certificate/UC-c82f6b0c-d371-46e5-a9e6-c2d55338007c/'
+    proofUrl: 'https://graphacademy.neo4j.com/c/757eeadf-22c7-473d-ac26-f21797a40206/'
   },
   { 
     title: 'Complete Generative AI Course With Langchain and Huggingface', 
